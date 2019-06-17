5de40976b8d35b5cc596405d0780ca5da55cd18a
salubrious-pipe-1423 <digregoriothomas@yahoo.com>
2019-06-17 00:18:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,2 +1,2 @@
-expect{introduction("Josh")}.to output("Hi, my name is Josh.\n")
-expect{introduction_with_language("Tom", "Python")}.to output("Hi, my name is Tom and I am learning to program in Python.\n")
+expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
+expect{introduction_with_language("Tom", "Python")}.to output("Hi, my name is Tom and I am learning to program in Python.\n").to_stdout
